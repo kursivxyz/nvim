5fe017c85217a294cfb8a9990fd3e366a3283b7a
palnyseth <paalnyseth@gmail.com>
2023-11-19 01:01:45 +0100

    migrated lsp-zero to v3

--- lua/kursiv/packer.lua
+++ lua/kursiv/packer.lua
@@ -47,26 +47,19 @@ return require('packer').startup(function(use)
 
         use {
             'VonHeikemen/lsp-zero.nvim',
-            branch = 'v1.x',
+            branch = 'v3.x',
             requires = {
+                --- Uncomment these if you want to manage LSP servers from neovim
+                 {'williamboman/mason.nvim'},
+                 {'williamboman/mason-lspconfig.nvim'},
+
                 -- LSP Support
                 {'neovim/nvim-lspconfig'},
-                {'williamboman/mason.nvim'},
-                {'williamboman/mason-lspconfig.nvim'},
-
                 -- Autocompletion
                 {'hrsh7th/nvim-cmp'},
-                {'hrsh7th/cmp-buffer'},
-                {'hrsh7th/cmp-path'},
-                {'saadparwaiz1/cmp_luasnip'},
                 {'hrsh7th/cmp-nvim-lsp'},
-                {'hrsh7th/cmp-nvim-lua'},
-
-                -- Snippets
                 {'L3MON4D3/LuaSnip'},
-                {'rafamadriz/friendly-snippets'},
             }
-        }
+        } use('ThePrimeagen/vim-be-good')
 
-        use('ThePrimeagen/vim-be-good')
     end)
